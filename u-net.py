bd209ad6544170ce542d0633482b42a624c87426
kazu-gor <58195776+kazu-gor@users.noreply.github.com>
2021-04-12 10:48:09 +0900

    updated

--- u-net.py
+++ u-net.py
@@ -20,64 +20,129 @@ def conv3x3_relu(num_channels, stride=1, padding=1):
                           nn.ReLU(inplace=True),
                           ])
 
+def double_conv(in_channels, out_channels, mid_channels=None, stride=1, padding=1):
+    if not mid_channels:
+        mid_channels = out_channels
+    return nn.Sequential(
+        nn.Conv2d(in_channels, mid_channels, stride=stride, padding=padding),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(mid_channels, out_channels, stride=stride, padding=padding),
+        nn.ReLU(inplace=True),
+    )
+
 def upconv(num_channels):
     return nn.Conv2d(num_channels, num_channels // 2, kernel_size=2)
 
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels, mid_channels=None, stride=1, padding=1):
+        if not mid_channels:
+            mid_channels = out_channels
+        self.convnet = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, stride=stride, padding=padding),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(mid_channels, out_channels, stride=stride, padding=padding),
+            nn.ReLU(inplace=True),
+        )
 
-class LeftSideNet(nn.Module):
-    def __init__(self, num_channels):
+    def forward(self, x):
+        return self.convnet(x)
+
+
+class ContractingPath(nn.Module):
+    def __init__(self, in_channels, out_channels):
         super().__init__()
-        self.convnet1 = conv3x3_relu(num_channels)
-        self.convnet2 = conv3x3_relu(num_channels)
+        self.convnet = DoubleConv(in_channels, out_channels)
         self.downsample = nn.MaxPool2d(kernel_size=2, stride=2)
 
     def forward(self, x):
-        for block in self.convnet1:
-            x = block(x)
-        for block in self.convnet2:
-            x = block(x)
-        cache = x
+        x = self.convnet(x)
         x = self.downsample(x)
-        return x, cache
+        return x
 
-
-class RightSideNet(nn.Module):
-    def __init__(self, num_channels, index):
+class ExpansivePath(nn.Module):
+    def __init__(self, in_channels, out_channels, mid_channels=None):
         super().__init__()
-        self.upconv = upconv(num_channels)
-        self.convnet1 = conv3x3_relu(num_channels // 2)
-        self.convnet2 = conv3x3_relu(num_channels // 2)
+        self.upconv = upconv(in_channels)
+        self.convnet = DoubleConv(in_channels, out_channels, mid_channels=mid_channels)
 
     def forward(self, x, cache):
         x = upconv(x)
         x = torch.cat([cache, x])
-        for block in self.convnet1:
-            x = block(x)
-        for block in self.convnet2:
-            x = block(x)
+        x = self.convnet(x)
         return x
-
 
 class UNet(nn.Module):
     def __init__(self, in_channels, num_classes):
         super().__init__()
         self.caches = []
 
-        self.left_side_first_block = left_side_first_block(in_channels)
-        self.leftconv = nn.ModuleList([LeftSideNet(2 ** i) for i in range(7, 11)])
-        self.rightconv = nn.ModuleList([RightSideNet(2 ** (10 - i), i) for i in range(4)])
+        self.input_conv = DoubleConv(in_channels, 2 ** 6, kernel_size=3, stride=1, padding=1)
+        self.left = nn.ModuleList([ContractingPath(2 ** i, 2 ** (i+1)) for i in range(6, 11)])
+        self.right = nn.ModuleList([ExpansivePath(2 ** i, 2 ** (i-1)) for i in range(10, 5, -1)])
         self.conv1x1 = nn.Conv2d(64, num_classes, kernel_size=1, stride=1, padding=1)
 
     def forward(self, x):
-        for block in self.left_side:
-            x = block(x)
-        for block in self.leftconv:
-            x, cache = block(x)
-            self.caches.append(cache)
-        for block, cache in zip(self.rightconv, caches):
-            x = block(x, cache)
+        x = self.input_conv(x)
+        
         x = self.conv1x1(x)
         return x
+
+# class LeftSideNet(nn.Module):
+#     def __init__(self, in_channels, out_channels):
+#         super().__init__()
+#         # self.convnet1 = conv3x3_relu(num_channels)
+#         self.convnet1 = DoubleConv(in_channels, out_channels)
+#         # self.convnet2 = conv3x3_relu(num_channels)
+#         self.convnet2 = DoubleConv(in_channels, out_channels)
+#         self.downsample = nn.MaxPool2d(kernel_size=2, stride=2)
+
+#     def forward(self, x):
+#         for block in self.convnet1:
+#             x = block(x)
+#         for block in self.convnet2:
+#             x = block(x)
+#         cache = x
+#         x = self.downsample(x)
+#         return x, cache
+
+
+# class RightSideNet(nn.Module):
+#     def __init__(self, num_channels, index):
+#         super().__init__()
+#         self.upconv = upconv(num_channels)
+#         self.convnet1 = conv3x3_relu(num_channels // 2)
+#         self.convnet2 = conv3x3_relu(num_channels // 2)
+
+#     def forward(self, x, cache):
+#         x = upconv(x)
+#         x = torch.cat([cache, x])
+#         for block in self.convnet1:
+#             x = block(x)
+#         for block in self.convnet2:
+#             x = block(x)
+#         return x
+
+
+# class UNet(nn.Module):
+#     def __init__(self, in_channels, num_classes):
+#         super().__init__()
+#         self.caches = []
+
+#         self.left_side_first_block = left_side_first_block(in_channels)
+#         self.leftconv = nn.ModuleList([LeftSideNet(2 ** i) for i in range(7, 11)])
+#         self.rightconv = nn.ModuleList([RightSideNet(2 ** (10 - i), i) for i in range(4)])
+#         self.conv1x1 = nn.Conv2d(64, num_classes, kernel_size=1, stride=1, padding=1)
+
+#     def forward(self, x):
+#         for block in self.left_side:
+#             x = block(x)
+#         for block in self.leftconv:
+#             x, cache = block(x)
+#             self.caches.append(cache)
+#         for block, cache in zip(self.rightconv, caches):
+#             x = block(x, cache)
+#         x = self.conv1x1(x)
+#         return x
 
 
 if __name__ == "__main__":
