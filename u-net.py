8dd83e0c2adf20870af9848c0da46ab5330f741a
kazu-gor <58195776+kazu-gor@users.noreply.github.com>
2021-04-12 15:43:16 +0900

    updated

--- u-net.py
+++ u-net.py
@@ -30,17 +30,15 @@ def double_conv(in_channels, out_channels, mid_channels=None, stride=1, padding=
         nn.ReLU(inplace=True),
     )
 
-def upconv(num_channels):
-    return nn.Conv2d(num_channels, num_channels // 2, kernel_size=2)
-
 class DoubleConv(nn.Module):
     def __init__(self, in_channels, out_channels, mid_channels=None, stride=1, padding=1):
+        super().__init__()
         if not mid_channels:
             mid_channels = out_channels
         self.convnet = nn.Sequential(
-            nn.Conv2d(in_channels, mid_channels, stride=stride, padding=padding),
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, stride=stride, padding=padding),
             nn.ReLU(inplace=True),
-            nn.Conv2d(mid_channels, out_channels, stride=stride, padding=padding),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, stride=stride, padding=padding),
             nn.ReLU(inplace=True),
         )
 
@@ -56,18 +54,26 @@ class ContractingPath(nn.Module):
 
     def forward(self, x):
         x = self.convnet(x)
-        x = self.downsample(x)
+        # x = self.downsample(x)
         return x
 
 class ExpansivePath(nn.Module):
     def __init__(self, in_channels, out_channels, mid_channels=None):
         super().__init__()
-        self.upconv = upconv(in_channels)
+        self.upconv = nn.Conv2d(in_channels, in_channels // 2, kernel_size=2)
         self.convnet = DoubleConv(in_channels, out_channels, mid_channels=mid_channels)
 
-    def forward(self, x, cache):
-        x = upconv(x)
-        x = torch.cat([cache, x])
+    def forward(self, cache, x):
+        x = self.upconv(x)
+        print(f"x.shape: {x.shape}")
+        print(f"cache.shape: {cache.shape}")
+        diffY = cache.size()[2] - x.size()[2]
+        diffX = cache.size()[3] - x.size()[3]
+
+        x = F.pad(x, [diffX // 2, diffX - diffX // 2,
+                      diffY // 2, diffY - diffY // 2])
+        x = torch.cat([cache, x], dim=1)
+        print(f"x.shape: {x.shape}")
         x = self.convnet(x)
         return x
 
@@ -76,14 +82,23 @@ class UNet(nn.Module):
         super().__init__()
         self.caches = []
 
-        self.input_conv = DoubleConv(in_channels, 2 ** 6, kernel_size=3, stride=1, padding=1)
-        self.left = nn.ModuleList([ContractingPath(2 ** i, 2 ** (i+1)) for i in range(6, 11)])
-        self.right = nn.ModuleList([ExpansivePath(2 ** i, 2 ** (i-1)) for i in range(10, 5, -1)])
+        self.input_conv = DoubleConv(in_channels, 2 ** 6, stride=1, padding=1)
+        self.left = nn.ModuleList([ContractingPath(2 ** i, 2 ** (i+1)) for i in range(6, 10)])
+        self.right = nn.ModuleList([ExpansivePath(2 ** i, 2 ** (i-1)) for i in range(10, 6, -1)])
         self.conv1x1 = nn.Conv2d(64, num_classes, kernel_size=1, stride=1, padding=1)
 
     def forward(self, x):
         x = self.input_conv(x)
-        
+        for block in self.left:
+            x = block(x)
+            self.caches.append(x)
+        del self.caches[-1]
+        self.caches.reverse()
+        for block, cache in zip(self.right, self.caches):
+            print(f"x.shaope:    {x.shape}")
+            print(f"cache.shape: {cache.shape}")
+            x = block(cache, x)
+        print(f"x.shape: {x.shape}")
         x = self.conv1x1(x)
         return x
 
@@ -146,5 +161,10 @@ class UNet(nn.Module):
 
 
 if __name__ == "__main__":
-    model = UNet(in_channels=1, num_classes=2)
+    model = UNet(in_channels=224, num_classes=2)
     print(model)
+
+    torch.manual_seed(0)
+    datas = torch.randn(64, 224, 3, 3)
+    y = model(datas)
+    make_dot(y, params=model.named_parameters)
